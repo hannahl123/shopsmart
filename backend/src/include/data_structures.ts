f3dc1ce7068444df39485b0d47d0bd1ca67e1e9c
Davis Liu <davisliu2006@gmail.com>
2024-10-31 22:33:05 -0400

    Priority queue

--- backend/src/include/data_structures.ts
+++ backend/src/include/data_structures.ts
@@ -16,3 +16,60 @@ export function Arr2d(sx: number, sy: number, v: any = 0): Array<Array<any>> {
     }
     return arr;
 }
+export function swap(arr: Array<any>, i: number, j: number) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = arr[i];
+}
+
+export class PriorityQueue<T> {
+    arr: Array<T>;
+    PriorityQueue(arr1: Array<T>) {
+        for (let i = 0; i < arr1.length; i++) {
+            this.arr = Arr(arr1.length);
+            this.arr[i] = arr1[i];
+        }
+    }
+
+    static root(x: number) {return (x-1)/2;}
+    static lnode(x: number) {return x*2+1;}
+    static rnode(x: number) {return x*2+2;}
+
+    size(): number {
+        return this.arr.length;
+    }
+    top(): T {
+        return this.arr[0];
+    }
+    push(x: T) {
+        let i = this.arr.length;
+        this.arr.push(x);
+        while (i > 0) {
+            let i0 = PriorityQueue.root(i);
+            if (this.arr[i] > this.arr[i0]) {
+                swap(this.arr, i, i0);
+                i = i0;
+            } else {
+                break;
+            }
+        }
+    }
+    pop() {
+        swap(this.arr, 0, this.arr.length-1);
+        this.arr.pop();
+        let i = 0;
+        while (true) {
+            let il = PriorityQueue.lnode(i);
+            let ir = PriorityQueue.rnode(i);
+            if (il < this.arr.length && this.arr[i] < this.arr[il]) {
+                swap(this.arr, i, il);
+                i = il;
+            } else if (ir < this.arr.length && this.arr[i] < this.arr[ir]) {
+                swap(this.arr, i, ir);
+                i = ir;
+            } else {
+                break;
+            }
+        }
+    }
+}

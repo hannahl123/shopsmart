f468b200093fa02896aaf94a342e9460093d7a0d
Hannah <53445855+hannahl123@users.noreply.github.com>
2024-11-06 19:22:20 -0500

    Created navbar

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,35 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 
-export default function RootLayout() {
+export default function TabLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          marginTop: 15,
+          marginBottom: 15, 
+          marginHorizontal: 10,
+          height: '10%',
+        }, 
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index" 
+        options={{
+          title: 'HOME'
+        }}
+      />
+
+      <Tabs.Screen 
+        name="trip" 
+        options={{
+          title: 'TRIP'
+        }}
+      />
+
+    </Tabs>
   );
 }
--- frontend/app/trip.tsx
+++ frontend/app/trip.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Trip() {
   return (
     <View
       style={{
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>哈喽姐姐 好爱你哦</Text>
+      <Text>hai</Text>
     </View>
   );
 }

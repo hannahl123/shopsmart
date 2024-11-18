c32dcc2ff3303fb7b41de4ef585f04480519f63c
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2024-11-18 16:28:09 -0500

    Get company and items

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,27 @@
 import express from "express";
+import { query } from "./db";
 
 const app = express();
-
 const PORT = 3000;
 
 app.get("/", (req: express.Request, res: express.Response) => {
-    // console.log("Hello world");
     res.send("Hello world");
+});
+
+app.get("/api/get-companies", async (req, res) => {
+    res.send(await query("SELECT * FROM companies"));
+});
+
+app.get("/api/company-items/:companyId", async (req, res) => {
+    const companyId = req.params.companyId;
+    const items = await query(`
+        SELECT product_id, company_items.name, price
+        FROM company_items
+        LEFT JOIN products
+            ON products.id = company_items.product_id
+        WHERE company_id = ${companyId};
+    `);
+    res.send(items);
 });
 
 app.listen(PORT, () => {
